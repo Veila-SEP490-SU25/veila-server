911878f69bc7bdd0f15ce02b4a939970c0c7ffcd
neru <ngocngocthuc@gmail.com>
2025-05-22 17:47:05 +0700

    chore: set timezone to Asia/Ho_Chi_Minh in main application file

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+process.env.TZ = 'Asia/Ho_Chi_Minh';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { HttpStatusCodeInterceptor } from '@/common/interceptors';
