617bd08f736b8af701cedc9181a7a2141f3ae788
Krit Dat <113153168+NHKritDat@users.noreply.github.com>
2025-06-30 16:39:22 +0700

    feat: Blog - Api cơ bản chưa có chức năng

--- src/app/blog/blog.controller.ts
+++ src/app/blog/blog.controller.ts
@@ -1,12 +1,50 @@
 import { ItemResponse, ListResponse } from '@/common/base';
-import { Blog } from '@/common/models';
-import { Controller } from '@nestjs/common';
+import { Blog, UserRole } from '@/common/models';
+import { Controller, Delete, Get, Patch, Post, Put, UseGuards } from '@nestjs/common';
 import { ApiExtraModels, ApiTags } from '@nestjs/swagger';
 import { BlogService } from './blog.service';
+import { AuthGuard } from '@/common/guards';
+import { Roles } from '@/common/decorators';
 
 @Controller('blogs')
 @ApiTags('Blog Controller')
 @ApiExtraModels(ItemResponse, ListResponse, Blog)
 export class BlogController {
   constructor(private readonly blogService: BlogService) {}
+
+  @Get()
+  async getBlogsForCustomer() {}
+
+  @Get(':id')
+  async getBlogForCustomer() {}
+
+  @Get('me')
+  @UseGuards(AuthGuard)
+  @Roles(UserRole.SHOP)
+  async getBlogsForOwner() {}
+
+  @Get(':id/me')
+  @UseGuards(AuthGuard)
+  @Roles(UserRole.SHOP)
+  async getBlogForOwner() {}
+
+  @Post('me')
+  @UseGuards(AuthGuard)
+  @Roles(UserRole.SHOP)
+  async createBlogForOwner() {}
+
+  @Put(':id/me')
+  @UseGuards(AuthGuard)
+  @Roles(UserRole.SHOP)
+  async updateBlogForOwner() {}
+
+  @Delete(':id/me')
+  @UseGuards(AuthGuard)
+  @Roles(UserRole.SHOP)
+  async removeBlogForOwner() {}
+
+  @Patch(':id/me')
+  @UseGuards(AuthGuard)
+  @Roles(UserRole.SHOP)
+  async restoreBlogForOwner() {}
 }
