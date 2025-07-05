2a89383d2296ffc66da43d4f9b70b980c5d72320
Krit Dat <113153168+NHKritDat@users.noreply.github.com>
2025-07-05 15:23:16 +0700

    feat: update SeedingModule to include additional service modules

--- src/app/seeding/seeding.module.ts
+++ src/app/seeding/seeding.module.ts
@@ -1,9 +1,25 @@
+import { AccessoryModule } from '@/app/accessory';
+import { BlogModule } from '@/app/blog';
+import { CategoryModule } from '@/app/category';
+import { DressModule } from '@/app/dress';
+import { PasswordModule } from '@/app/password';
 import { SeedingService } from '@/app/seeding/seeding.service';
+import { ServiceModule } from '@/app/service';
+import { ShopModule } from '@/app/shop';
 import { UserModule } from '@/app/user';
 import { Module } from '@nestjs/common';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    PasswordModule,
+    AccessoryModule,
+    BlogModule,
+    CategoryModule,
+    DressModule,
+    ServiceModule,
+    ShopModule,
+  ],
   providers: [SeedingService],
 })
 export class SeedingModule {}
